10b129c613baa5c76adbdd6d56152b4c800834a7
Fabio Nunes de Paula <146302081+fabionunesdepaula@users.noreply.github.com>
2024-04-10 10:38:55 -0300

    Update ConteudoPrincipal.vue

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -78,6 +78,7 @@ export default {
 <style scoped>
 .conteudo-principal {
   width: 100vw; /* A largura é a da viewport para ocupar toda a largura da tela */
+  max-width: 98%;
   margin-left: 0.5vw;
   padding: 6.5rem 0.05rem; /* Remove padding lateral, mantendo vertical */
   border-radius: 3.75rem 3.75rem 0 0;
