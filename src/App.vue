a6633dca52cc1663bac90fff87b9d7318c5b7903
Fabio Nunes de Paula <146302081+fabionunesdepaula@users.noreply.github.com>
2024-04-10 10:40:13 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@ export default {
 .main-container {
   display: flex;
   flex-direction: column;
-  width: 99vw; /* A largura é a da viewport para evitar deslocamento */
+  width: 98vw; /* A largura é a da viewport para evitar deslocamento */
   margin-left: -10%; /* Remove qualquer margem padrão */
   padding: 0; /* Remove qualquer padding padrão */
   /*overflow-x: hidden; /* Previne rolagem horizontal indesejada */
